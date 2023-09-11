88324aa1b54f7f060c84502bc7ec8c202f93f81d
StepC <step.coquelle@gmail.com>
2023-09-11 14:10:12 +0200

    new project

--- src/test/java/StepsDefs/MyStepdefs.java
+++ src/test/java/StepsDefs/MyStepdefs.java
@@ -1,10 +1,14 @@
+package StepsDefs;
+
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.serenitybdd.core.pages.PageObject;
 
-public class MyStepdefs {
+public class MyStepdefs extends PageObject {
     @Given("the user is on {string}")
     public void theUserIsOn(String arg0) {
+        openAt(arg0);
     }
 
     @When("he click on accept all")
